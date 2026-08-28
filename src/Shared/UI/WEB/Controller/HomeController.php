b5c97917dfbea7de08e82b8c76316540068e3d88
Daniele Rastelli <daniele.rastelli@gmail.com>
2026-08-28 12:38:39 +0200

    set login page

--- src/Shared/UI/WEB/Controller/HomeController.php
+++ src/Shared/UI/WEB/Controller/HomeController.php
@@ -13,7 +13,6 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
         /** @var User $user */
